5972df1ddf88380d0522dfa089975caaab430fe4
Kou-shik2004 <koushik20040804@gmail.com>
2024-06-13 21:51:57 +0530

    modified launch file according to humble

--- agv2_description/launch/description.launch.py
+++ agv2_description/launch/description.launch.py
@@ -1,97 +1,47 @@
-# Copyright (c) 2021 Juan Miguel Jimeno
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http:#www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
+from ament_index_python.packages import get_package_share_directory
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, EnvironmentVariable
-from launch.conditions import IfCondition
+from launch.substitutions import Command, LaunchConfiguration
+
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-    robot_base = os.getenv('AGV2_BASE')
+    agv2_description_dir = get_package_share_directory('agv2_description')
 
-    urdf_path = PathJoinSubstitution(
-        [FindPackageShare("agv2_description"), "urdf", f"{robot_base}.urdf.xacro"]
+    model_arg = DeclareLaunchArgument(name='model', default_value=os.path.join(
+                                        agv2_description_dir, 'urdf', '2wd.urdf.xacro'
+                                        ),
+                                      description='Absolute path to robot urdf file')
+
+    robot_description = ParameterValue(Command(['xacro ', LaunchConfiguration('model')]),
+                                       value_type=str)
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': robot_description}]
     )
 
-    rviz_config_path = PathJoinSubstitution(
-        [FindPackageShare('agv2_description'), 'rviz', 'description.rviz']
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui'
+    )
+
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(agv2_description_dir, 'rviz', 'desp.rviz')],
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            name='urdf', 
-            default_value=urdf_path,
-            description='URDF path'
-        ),
-        
-        DeclareLaunchArgument(
-            name='publish_joints', 
-            default_value='true',
-            description='Launch joint_states_publisher'
-        ),
-
-        DeclareLaunchArgument(
-            name='rviz', 
-            default_value='false',
-            description='Run rviz'
-        ),
-
-        DeclareLaunchArgument(
-            name='use_sim_time', 
-            default_value='false',
-            description='Use simulation time'
-        ),
-
-        Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
-            condition=IfCondition(LaunchConfiguration("publish_joints")),
-            parameters=[
-                {'use_sim_time': LaunchConfiguration('use_sim_time')}
-            ]
-        ),
-
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[
-                {
-                    'use_sim_time': LaunchConfiguration('use_sim_time'),
-                    'robot_description': Command(['xacro ', LaunchConfiguration('urdf')])
-                }
-            ]
-        ),
-
-        Node(
-            package='rviz2',
-            executable='rviz2',
-            name='rviz2',
-            output='screen',
-            arguments=['-d', rviz_config_path],
-            condition=IfCondition(LaunchConfiguration("rviz")),
-            parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
-        )
+        model_arg,
+        joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        rviz_node
     ])
-
-#sources: 
-#https://navigation.ros.org/setup_guides/index.html#
-#https://answers.ros.org/question/374976/ros2-launch-gazebolaunchpy-from-my-own-launch-file/
-#https://github.com/ros2/rclcpp/issues/940
